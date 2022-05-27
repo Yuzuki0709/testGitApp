24edd88422abb5bfd060fd9b605f29f42b304540
Yuzuki0709 <ryuto2001@ezweb.ne.jp>
2022-05-27 21:06:28 +0900

    TestGitApp用のRepository作成

---
+++ ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TestGitApp
+//
+//  Created by 岩本竜斗 on 2022/05/27.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
